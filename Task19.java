4eedad9c9b5a3c0d27275e1a5f925040da0919df
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:17:11 +0530

    Add files via upload

---
+++ Task19.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the text");
+		char[] letters = scanner.nextLine().toCharArray();
+		System.out.println("Reverse Text");
+	}
+
+}
