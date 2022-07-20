721a5962a63589c75be3cf2acb05337548db3dba
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:17:31 +0530

    Add files via upload

---
+++ Task20.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value");
+int a=sc.nextInt();
+if(a>0) {
+	System.out.println(a+ "is a postive number");
+}
+else
+{
+	System.out.println(a+ "is a negative number");
+}
+	}
+
+}
